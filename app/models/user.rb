6a8e71c922ad59686b47b0dc474ad1c49f0b6659
Grace Durant <gdurant34@gmail.com>
2022-12-18 14:15:20 -0800

    setup user backed 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :user_relationships
+    has_many :relationships, through: :user_relationships
+    has_many :activities
+    has_many :dayts
+    has_many :comments
 end
