6fb956f36812976fb21ebc450524edc08f89dba7
Grace Durant <gdurant34@gmail.com>
2022-12-19 13:49:44 -0600

    Building out Dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,22 @@
 import React from "react";
+import DatesPage from "./DatesPage";
 
 
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return(
+    <div>
+      <section>
+        <h3>Upcoming Dates</h3>
+        <DatesPage />
+      </section>
+      <section>
+        <h3>Suggested Dates</h3>
+      </section>
+      <section>
+        <h3>Messages</h3>
+      </section>
+    </div>
+    );
 };
 
 export default Dashboard;
