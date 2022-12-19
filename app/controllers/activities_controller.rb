961ddd2b959593e653c7bb645938e6a3ac8fd48a
Grace Durant <gdurant34@gmail.com>
2022-12-18 20:16:30 -0800

    updated dayts controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,20 @@
-class ActivitiesController < ApplicationController
+class DaytsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_uprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    def index
+        dayts = Dayt.all
+        render json: dayts, status: :ok
+    end
+    
+    private
+    
+    def render_uprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+    
+    def render_not_found_response
+        render json: { errors: "Date not found" }, status: :not_found
+    end
+    
 end
