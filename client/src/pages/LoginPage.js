819ee75bf08459fbbac427318ac00b6c1180b1c2
Grace Durant <gdurant34@gmail.com>
2022-12-19 14:51:43 -0600

    build out basic login structure

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,7 +2,25 @@ import React from "react";
 
 
 function LoginPage() {
-  return <h1>LoginPage</h1>;
+  return (
+    <div className='login-form-div'>
+        <form className='login-form'>
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" name="username" />
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="pass" name="password" />
+            </div>
+            <div>
+                <button className='button' type="submit" value="Login">
+                Login
+                </button>
+            </div>
+        </form>
+    </div>
+)
 };
 
 export default LoginPage;
