c0d6ba1e326d00765af0fece598645b059ed7d69
Grace Durant <gdurant34@gmail.com>
2022-12-19 13:45:47 -0600

    added Relationship index route

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,19 @@
 class RelationshipsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_uprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    def index
+        relationships = Relationship.all
+        render json: relationships, status: :ok
+    end
+    
+    private
+    
+    def render_uprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+    
+    def render_not_found_response
+        render json: { errors: "Relationship not found" }, status: :not_found
+    end
 end
