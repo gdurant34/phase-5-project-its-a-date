76a891e5e355ca84e7f5571331173d90c4e9b392
Grace Durant <gdurant34@gmail.com>
2022-12-19 13:47:09 -0600

    added UserRelationship index route

--- app/controllers/user_relationships_controller.rb
+++ app/controllers/user_relationships_controller.rb
@@ -1,2 +1,19 @@
 class UserRelationshipsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_uprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    def index
+        user_relationship = UserRelationship.all
+        render json: user_relationship, status: :ok
+    end
+    
+    private
+    
+    def render_uprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+    
+    def render_not_found_response
+        render json: { errors: "UserRelationship not found" }, status: :not_found
+    end
 end
