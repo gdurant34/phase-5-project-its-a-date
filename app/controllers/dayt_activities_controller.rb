bf16335dfd390fec8b4250406dca921d7c624bd2
Grace Durant <gdurant34@gmail.com>
2022-12-19 13:44:58 -0600

    added DaytActivities index route

--- app/controllers/dayt_activities_controller.rb
+++ app/controllers/dayt_activities_controller.rb
@@ -1,2 +1,19 @@
 class DaytActivitiesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_uprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    def index
+        dayt_activities = DaytActivity.all
+        render json: dayt_activities, status: :ok
+    end
+    
+    private
+    
+    def render_uprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+    
+    def render_not_found_response
+        render json: { errors: "DateActivity not found" }, status: :not_found
+    end
 end
