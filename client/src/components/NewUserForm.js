c250f42cc0f33b2e1117ff7677e15e63806d9e2c
Grace Durant <gdurant34@gmail.com>
2022-12-20 17:43:05 -0600

    building out newUserForm

--- client/src/components/NewUserForm.js
+++ client/src/components/NewUserForm.js
@@ -1,63 +1,79 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import { currentUserState, userFormDataState, errorsState } from '../recoil/atoms';
+import { useNavigate } from 'react-router-dom';
 
 const NewUserForm = () => {
-    
-    // function handleSubmit(e){
-    //     e.preventDefault();
-    //     const user = {
-    //         first_name: formData.firstName,
-    //         last_name: formData.lastName,
-    //         age: formData.age,
-    //         username: formData.username,
-    //         password: formData.password,
-    //         password_confirmation: formData.passwordConfirmation
-    //     }
-    //     fetch(`/users`,{
-    //         method: "POST",
-    //         headers:{'Content-Type':'application/json'},
-    //         body:JSON.stringify(user)
-    //     })
-    //     .then(res => {
-    //         if(res.ok){
-    //             res.json().then(setCurrentUser)
-    //         } else {
-    //             res.json().then( e => setErrors(Object.entries(e.error).flat()))
-    //         }
-    //     })
-    //     .then(navigate('/home'))
-    // }
+    const [userFormData, setUserFormData] = useRecoilState(userFormDataState);
+    const setCurrentUserState = useRecoilState(currentUserState);
+    const setErrorsState = useRecoilState(errorsState)
+
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        setUserFormData((userFormData) => ({
+            ...userFormData,
+            [name] : value
+        }));
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const user = {
+            first_name: userFormData.firstName,
+            last_name: userFormData.lastName,
+            age: userFormData.age,
+            username: userFormData.userName,
+            email: userFormData.email
+        }
+        fetch(`/users`,{
+            method: "POST",
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify(user)
+        })
+        .then(res => {
+            if(res.ok){
+                res.json().then(setCurrentUserState)
+            } else {
+                res.json().then( e => setErrorsState(Object.entries(e.error).flat()))
+            }
+        })
+        .then(navigate('/'))
+    }
     
     return(
         <div className="New-user-card">
-            <form >
+            <form onSubmit={handleSubmit} >
                 <div>
                     <label htmlFor="firstName">First Name:</label>
-                    <input type="text" name="firstName"  />
+                    <input type="text" name="firstName" value={userFormData.firstName} onChange={(e)=> handleChange(e)} />
                 </div>
                 <div>
                     <label htmlFor="lastName">Last Name:</label>
-                    <input type="text" name="lastName"  />
+                    <input type="text" name="lastName" value={userFormData.lastName} onChange={(e)=> handleChange(e)} />
                 </div>
                 <div>
                     <label htmlFor="userName">Username:</label>
-                    <input type="text" name="userName"  />
+                    <input type="text" name="userName" value={userFormData.userName} onChange={(e)=> handleChange(e)} />
                 </div>
                 <div>
                     <label htmlFor="age">Age:</label>
-                    <input type="integer" name="age"  />
+                    <input type="integer" name="age" value={userFormData.age} onChange={(e)=> handleChange(e)} />
                 </div>
                 <div>
                     <label htmlFor="email">Email:</label>
-                    <input type="string" name="email"  />
+                    <input type="string" name="email" value={userFormData.email} onChange={(e)=> handleChange(e)} />
                 </div>
-                <div>
+                {/* <div>
                     <label htmlFor="password">Password:</label>
                     <input type="password" name="password"  />
                 </div>
                 <div>
                     <label htmlFor="password_confirmation">Confirm Password:</label>
                     <input type="password" id="password_confirmation" name="passwordConfirmation" />
-                </div>
+                </div> */}
                 <div>
                     <input className='button' type="submit" />
                 </div>
