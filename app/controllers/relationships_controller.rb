23fff9f46d182d13263762f888bed5f3123ad05f
Grace Durant <gdurant34@gmail.com>
2022-12-20 20:08:05 -0600

    added crud for relationship backend

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,8 +6,38 @@ class RelationshipsController < ApplicationController
         relationships = Relationship.all
         render json: relationships, status: :ok
     end
+
+    def show 
+        relationship = find_relationship
+        render json: relationship, status: :ok
+    end
+
+    def create
+        relationship = Relationship.create!(relationship_params)
+        render json: relationship, status: :created
+    end
+
+    def update
+        relationship = find_relationship
+        relationship.update!(relationship_params)
+        render json: relationship, status: :accepted
+    end
     
+    def destroy
+        relationship = find_relationship
+        relationship.destroy
+        head :no_content
+    end
+
     private
+
+    def find_relationship
+        Relationship.find(params[:id])
+    end
+
+    def relationship_params
+        params.permit(:relationship_type)
+    end
     
     def render_uprocessable_entity_response(exception)
         render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
