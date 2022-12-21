610282c427b1024496eb390f41e8bb7bee8a1dfc
Grace Durant <gdurant34@gmail.com>
2022-12-21 01:47:21 -0600

    added modal to date page

--- client/src/pages/DatesPage.js
+++ client/src/pages/DatesPage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from 'react';
 import ActivitiesDisplay from '../components/ActivitiesDisplay'
 import DateDisplay from "../components/DateDisplay";
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+import NewDateForm from '../components/NewDateForm';
+
 
 
 function DatesPage() {
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
   return(
     <div>
       <section>
@@ -13,7 +22,10 @@ function DatesPage() {
       <section>
         <h3>Dates</h3>
         <DateDisplay />
-        <button>Add New</button>
+        <button onClick={onOpenModal}>Add New</button>
+        <Modal open={open} onClose={onCloseModal} center>
+          <NewDateForm />
+        </Modal>
       </section>
     </div>
   );
