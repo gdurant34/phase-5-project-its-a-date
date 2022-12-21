93e8b15dc89460b6b9fb053da342e77b8d84f3b1
Grace Durant <gdurant34@gmail.com>
2022-12-21 14:36:05 -0600

    working on useEffect for activities

--- client/src/components/ActivitiesDisplay.js
+++ client/src/components/ActivitiesDisplay.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import { Card } from "semantic-ui-react";
 
 
 function ActivitiesDisplay() {
+
+  const [activities, setActivities] = useState({})
+
+  useEffect(() => {
+    fetch('/activities')
+      .then(r => r.json())
+      .then(setActivities)
+  }, [])
+  console.log(activities)
+
   return(
     <div>
     
