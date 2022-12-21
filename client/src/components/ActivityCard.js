05704346a189a392b15d3e59cfdb2f56d1c19c4e
Grace Durant <gdurant34@gmail.com>
2022-12-21 14:35:31 -0600

    basic structure of activity card

---
+++ client/src/components/ActivityCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { Card } from "semantic-ui-react";
+
+
+function ActivityCard() {
+  return(
+    <div>
+    
+    </div>
+    );
+};
+
+export default ActivityCard;
