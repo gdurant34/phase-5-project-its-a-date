06fca83a5a73e824daf3912d4b692ff353e944e6
Grace Durant <gdurant34@gmail.com>
2022-12-20 19:05:41 -0600

    added CRU routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,31 @@ class UsersController < ApplicationController
         render json: user, status: :ok
     end
     
+    def show
+        user = find_user
+        render json: user, status: :ok
+    end
+    
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    def update
+        user = find_user
+         user.update!(user_params)
+        render json: user, status: :accepted
+    end
+    
     private
+
+    def find_user
+        User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:first_name, :last_name, :user_name, :age, :email)
+    end
     
     def render_uprocessable_entity_response(exception)
         render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
