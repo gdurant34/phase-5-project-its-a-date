ed01964ee515e9cd19f2df6d6811e846b6b98462
Grace Durant <gdurant34@gmail.com>
2022-12-20 19:06:30 -0600

    added CRUD actions for activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,8 +6,38 @@ class ActivitiesController < ApplicationController
         activities = Activity.all
         render json: activities, status: :ok
     end
+
+    def show 
+        activity = find_activity
+        render json: activity, status: :ok
+    end
+
+    def create
+        activity = Activity.create!(activity_params)
+        render json: activity, status: :created
+    end
+
+    def update
+        activity = find_activity
+        activity.update!(activity_params)
+        render json: activity, status: :accepted
+    end
+
+    def destroy
+        activity = find_activity
+        activity.destroy
+        head :no_content
+    end
     
     private
+
+    def find_activity
+        Activity.find(params[:id])
+    end
+
+    def activity_params
+        params.permit(:title, :category, :location, :description, :image, :est_price, :user_id, :relationship_id)
+    end
     
     def render_uprocessable_entity_response(exception)
         render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,3 +1,5 @@
 class ActivitySerializer < ActiveModel::Serializer
   attributes :id, :title, :category, :location, :description, :image, :est_price
+  belongs_to :user
+  belongs_to :relationship_id
 end
