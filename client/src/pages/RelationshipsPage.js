2fb960852a68a02a577c86fac770dc982f39f029
Grace Durant <gdurant34@gmail.com>
2022-12-21 01:47:52 -0600

    added modal to relationship page

--- client/src/pages/RelationshipsPage.js
+++ client/src/pages/RelationshipsPage.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+import NewRelationshipForm from '../components/NewRelationshipForm'
 
 
 function RelationshipsPage() {
+
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
   return(
     <div>
       <section>You & Romantic(name)</section>
       <section>You & Friend(name)</section>
       <section>You & Mother(name)</section>
       <section>
-        <button>New Relationship</button>
+      <button onClick={onOpenModal}>New Relationship</button>
+        <Modal open={open} onClose={onCloseModal} center>
+          <NewRelationshipForm />
+        </Modal>
         <br/>
         <button>Delete Relationship</button>
       </section>
