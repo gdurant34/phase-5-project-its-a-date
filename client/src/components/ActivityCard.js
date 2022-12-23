8fea272cda5941e166f2ee413abd25edf8b23869
Grace Durant <gdurant34@gmail.com>
2022-12-22 19:08:29 -0600

    edit activity card

--- client/src/components/ActivityCard.js
+++ client/src/components/ActivityCard.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
+import { Modal } from 'react-responsive-modal';
 import './ActivityCard.css'
+import { activityUpdateModalStateAtom } from '../recoil/atoms'
+import { useRecoilState } from 'recoil'
+import UpdateActivityForm from "./UpdateActivityFrom";
 
 function ActivityCard({ activity }) {
+    const [open, setOpen] = useRecoilState(activityUpdateModalStateAtom);
+
+    const onOpenModal = () => setOpen(true);
+    const onCloseModal = () => setOpen(false);
 
     // console.log(activity)
 
@@ -10,13 +18,19 @@ function ActivityCard({ activity }) {
     return (
         <Card>
             <Image src={activity.image} />
-            <div>
+            <section>
                 <Card.Content
                     header={activity.title}
                     meta={activity.category}
                     description={activity.description}
                 />
-            </div>
+            </section>
+            <section>
+                <button onClick={onOpenModal}>Edit</button>
+                <Modal open={open} onClose={onCloseModal} center>
+                    <UpdateActivityForm />
+                </Modal>
+            </section>
         </Card>
     );
 };
