a82f7ca06d1d38bf7ba3372931c99bf5309dda3e
Grace Durant <gdurant34@gmail.com>
2022-12-23 18:08:34 -0600

    working on add activity to date dropdown

--- client/src/components/DateCard.js
+++ client/src/components/DateCard.js
@@ -5,15 +5,18 @@ import DateActivities from "./DateActivities";
 import UpdateDateForm from "./UpdateDateFrom";
 import { Modal } from 'react-responsive-modal';
 import DeleteDateForm from "./DeleteDateForm";
+import DropdownActivityToDate from "./DropdownActivityToDate";
 
 
 function DateCard({ date }) {
 
     const [openEdit, setEditOpen] = useState(false);
     const [openDelete, setDeleteOpen] = useState(false);
+    const [openAddActivity, setOpenAddActivity] = useState(false);
+
     const handleEditModal = () => setEditOpen(!openEdit);
     const handleDeleteModal = () => setDeleteOpen(!openDelete)
-
+    const handleAddActivityModal = () => setOpenAddActivity(!openAddActivity)
 
     const activities = date.activities.map(activity => (
         <DateActivities key={activity.id} activity={activity} />
@@ -35,12 +38,19 @@ function DateCard({ date }) {
                 <button onClick={handleEditModal}>Edit</button>
                 <Modal open={openEdit} onClose={handleEditModal} center>
                     <UpdateDateForm date={date} setOpen={setEditOpen} open={openEdit} />
-                </Modal> <section>
+                </Modal>
+            </section>
+            <section>
+                <button onClick={handleAddActivityModal}>Add Activity</button>
+                <Modal open={openAddActivity} onClose={handleAddActivityModal} center>
+                    <DropdownActivityToDate date={date} setOpen={setOpenAddActivity} open={openAddActivity} />
+                </Modal>
+            </section>
+            <section>
                 <button onClick={handleDeleteModal}>X</button>
                 <Modal open={openDelete} onClose={handleDeleteModal} center>
                     <DeleteDateForm date={date} setOpen={setDeleteOpen} open={openDelete} />
                 </Modal>
-            </section>
             </section>
         </Card>
     );
---
+++ client/src/components/DropdownActivityToDate.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { activitiesStateAtom } from "../recoil/atoms";
+import { useRecoilState } from 'recoil';
+import { Dropdown } from 'semantic-ui-react'
+
+
+const DropdownActivityToDate = ({ date, setOpen, open }) => {
+
+  const [activities, setActivities] = useRecoilState(activitiesStateAtom)
+
+  const handleDropdownChange = (e) => {
+    //     const value = e.target.value;
+    //     const name = e.target.name;
+    //     setRelationshipFormData((relationshipFormData) => ({
+    //         ...relationshipFormData,
+    //         [name]: value
+    //     }))
+  }
+
+  const options = activities.map(activity => {
+    return (
+      { key: activity.title, text: activity.title, value: activity.title }
+    )
+  })
+
+  return (
+    <div>
+        <Dropdown placeholder='Select' fluid multiple selection options={options} />
+    </div>
+  );
+};
+export default DropdownActivityToDate;
