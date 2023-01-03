eb576feeeda0d9329b3cc7ba3615447201da079e
Grace Durant <gdurant34@gmail.com>
2023-01-03 13:16:26 -0800

    activity dropdown

--- client/src/components/DropdownActivityToDate.js
+++ client/src/components/DropdownActivityToDate.js
@@ -1,52 +1,58 @@
-import React, { useState } from "react";
-import { activitiesStateAtom } from "../recoil/atoms";
-import { useRecoilState } from 'recoil';
-import Select from 'react-select';
+// import React, { useEffect, useState } from "react";
+// import { activitiesStateAtom } from "../recoil/atoms";
+// import { datesStateAtom } from "../recoil/atoms";
+// import { useRecoilState, useResetRecoilState } from 'recoil';
+// import Select from 'react-select';
 
 
-const DropdownActivityToDate = ({ date, setOpen, open }) => {
+// const DropdownActivityToDate = ({ date, setOpen, open }) => {
 
-  const [activities, setActivities] = useRecoilState(activitiesStateAtom);
-  const [selected, setSelected] = useState([]);
+//   const [activities, setActivities] = useRecoilState(activitiesStateAtom);
+//   const setDates = useResetRecoilState(datesStateAtom);
+//   const [selected, setSelected] = useState([]);
 
-  // const handleDropdownChange = (e) => {
-  //   //     const value = e.target.value;
-  //   //     const name = e.target.name;
-  //   //     setRelationshipFormData((relationshipFormData) => ({
-  //   //         ...relationshipFormData,
-  //   //         [name]: value
-  //   //     }))
-  // }
 
-  const handleDropdownChange = (e) => {
-    setSelected(e)
-  }
+//   const handleDropdownChange = (e) => {
+//     setSelected(e)
+//     // filter out duplicates then set selected
+//   }
 
-  const options = activities.map(activity => {
-    return (
-      { value: activity.id, label: activity.title }
-    )
-  })
+//   const options = activities.map(activity => {
+//     return (
+//       { value: activity.id, label: activity.title }
+//     )
+//   })
 
-const handleClick = () => {
-  const dateActivity = {
-    
-  }
-}
+// const handleClick = () => {
+//   selected.map(activity => {
+//     const dateActivity = {
+//       dayt_id: date.id,
+//       activity_id: activity.value
+//     }
 
-  return (
-    <div>
-        <h3>Pick Your Activity / Activities</h3>
-        <Select
-          isSearchable={true}
-          isMulti={true}
-          value={selected}
-          onChange={handleDropdownChange}
-          options={options}
-        />
-        <button onClick={handleClick}>Save</button>
-    </div>
-  );
-};
-export default DropdownActivityToDate;
+//     fetch(`/dayt_activities`, {
+//       method: "POST",
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify(dateActivity)
+//     })
+//       .then(r => r.json())
+//       .then(setOpen(!open))
+//   })
+// }
+
+//   return (
+//     <div>
+//         <h3>Pick Your Activity / Activities</h3>
+//         <Select
+//           isSearchable={true}
+//           isMulti={true}
+//           value={selected}
+//           onChange={handleDropdownChange}
+//           options={options}
+//         />
+//         <button onClick={handleClick}>Save</button>
+//     </div>
+//   );
+// };
+// export default DropdownActivityToDate;
 
