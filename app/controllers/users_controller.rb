d17d46ac17d5724361a5b5cd4127179039535b3c
Grace Durant <gdurant34@gmail.com>
2023-01-03 22:08:34 -0800

    updated user controller for current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-    rescue_from ActiveRecord::RecordInvalid, with: :render_uprocessable_entity_response
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    wrap_parameters format: []
+    skip_before_action :authorized, only: [:create]
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     
     def index
         user = User.all
@@ -8,19 +9,18 @@ class UsersController < ApplicationController
     end
     
     def show
-        user = find_user
-        render json: user, status: :ok
+        render json: current_user
     end
     
     def create
         user = User.create!(user_params)
-        render json: user, status: :created
+        render json: current_user
     end
 
     def update
         user = find_user
          user.update!(user_params)
-        render json: user, status: :accepted
+        render json: user
     end
     
     private
@@ -30,14 +30,11 @@ class UsersController < ApplicationController
     end
 
     def user_params
-        params.permit(:first_name, :last_name, :user_name, :age, :email)
+        params.permit(:first_name, :last_name, :user_name, :age, :email, :password, :password_confirmation)
     end
     
     def render_uprocessable_entity_response(exception)
         render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
     end
-    
-    def render_not_found_response
-        render json: { errors: "User not found" }, status: :not_found
-    end
+
 end
