8a46d0f092cbcab076103012a4fb804689c483a9
Grace Durant <gdurant34@gmail.com>
2023-01-05 09:49:46 -0800

    date card styling

--- client/src/components/DateCard.js
+++ client/src/components/DateCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './ActivityCard.css'
+import './DateCard.css';
 import { Card } from 'semantic-ui-react';
 import DateActivities from "./DateActivities";
 import UpdateDateForm from "./UpdateDateFrom";
@@ -35,19 +35,19 @@ function DateCard({ date }) {
                 </Card.Content>
             </section>
             <section>
-                <button onClick={handleEditModal}>Edit</button>
+                <button id='edit-btn' onClick={handleEditModal}>Edit</button>
                 <Modal open={openEdit} onClose={handleEditModal} center>
                     <UpdateDateForm date={date} setOpen={setEditOpen} open={openEdit} />
                 </Modal>
             </section>
             <section>
-                <button onClick={handleAddActivityModal}>Add Activity</button>
+                <button id='add-activity-btn' className='card-btns' onClick={handleAddActivityModal}>Add Activity</button>
                 <Modal classNames='add-avtivity-modal' open={openAddActivity} onClose={handleAddActivityModal} center>
                     <DropdownActivityToDate date={date} setOpen={setOpenAddActivity} open={openAddActivity} />
                 </Modal>
             </section>
             <section>
-                <button onClick={handleDeleteModal}>X</button>
+                <button id='delete-btn' onClick={handleDeleteModal}>X</button>
                 <Modal open={openDelete} onClose={handleDeleteModal} center>
                     <DeleteDateForm date={date} setOpen={setDeleteOpen} open={openDelete} />
                 </Modal>
