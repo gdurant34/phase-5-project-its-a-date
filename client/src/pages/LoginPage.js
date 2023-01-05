07f7049ff53f01567bf30c25e2bdc4280badcd81
Grace Durant <gdurant34@gmail.com>
2023-01-04 18:49:04 -0800

    changed to ES6 arrow func

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -6,7 +6,7 @@ import { currentUserStateAtom } from "../recoil/atoms"
 
 
 
-function LoginPage() {
+const LoginPage = () => {
 
     const navigate = useNavigate();
     const setCurrentUser = useSetRecoilState(currentUserStateAtom)
@@ -24,7 +24,7 @@ function LoginPage() {
     const handleSubmit = (e) => {
         e.preventDefault()
         const loginParams = {
-            user_name: username, 
+            user_name: username,
             password: password
         }
 
@@ -35,30 +35,30 @@ function LoginPage() {
             },
             body: JSON.stringify(loginParams)
         })
-        .then(res => res.json())
-        .then(setCurrentUser)
-        .then(navigate('/dashboard'));
+            .then(res => res.json())
+            .then(setCurrentUser)
+            .then(navigate('/dashboard'));
     }
 
-  return (
-    <div className='login-form-div'>
-    <form className='login-form' onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" value={username} onChange={(e) => handleUsernameChange(e)} />
+    return (
+        <div className='login-form-div'>
+            <form className='login-form' onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" value={username} onChange={(e) => handleUsernameChange(e)} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="pass" name="password" value={password} onChange={(e) => handlePasswordChange(e)} />
+                </div>
+                <div>
+                    <button className='button' type="submit" value="Login">
+                        Login
+                    </button>
+                </div>
+            </form>
         </div>
-        <div>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="pass" name="password" value={password} onChange={(e) => handlePasswordChange(e)} />
-        </div>
-        <div>
-            <button className='button' type="submit" value="Login">
-            Login
-            </button>
-        </div>
-    </form>
-</div>
-)
+    )
 };
 
 export default LoginPage;
