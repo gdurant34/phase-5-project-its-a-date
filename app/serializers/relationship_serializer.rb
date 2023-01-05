2db49d42205fddc05a2077157ab319c67c7da5cc
Grace Durant <gdurant34@gmail.com>
2023-01-04 23:13:23 -0800

    added has many through on serializers

--- app/serializers/relationship_serializer.rb
+++ app/serializers/relationship_serializer.rb
@@ -1,3 +1,4 @@
 class RelationshipSerializer < ActiveModel::Serializer
   attributes :id, :relationship_type, :name, :email
+  has_many :users, serializer: UserSerializer
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :user_name, :age, :email
+  has_many :relationships, serializer: RelationshipSerializer
 end
